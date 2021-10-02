a4abd09130bc69309a8db1dfbb728723fa543e2b
Lorenzo Amata <lorenzoamata@gmail.com>
2021-10-02 13:08:27 +0200

    Initial commit: basic functions and connection w\ db

---
+++ src/Employee.java
@@ -0,0 +1,148 @@
+import net.proteanit.sql.DbUtils;
+
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.sql.*;
+
+
+public class Employee {
+    private JPanel Main;
+
+    private JTable table1;
+
+    private JLabel Visualize;
+    private JLabel Title;
+    private JLabel Mobile;
+    private JLabel salarytext;
+    private JLabel nametext;
+
+    private JTextField salarytextfield;
+    private JTextField mobiletextfield;
+    private JTextField nametextfield;
+    private JTextField searchtextfield;
+
+    private JButton searchbutton;
+    private JButton updatebutton;
+    private JButton saveButton1;
+    private JButton deleteButton;
+    private JTextField salaryupdatefield;
+    private JTextField idupdatefield;
+
+    //main function that sets the frame visible
+    public static void main(String[] args) {
+        JFrame frame = new JFrame("Employee");
+        frame.setContentPane(new Employee().Main);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.pack();
+        frame.setVisible(true);
+    }
+
+    //creation of the variable needed for creating a sql connection via driver
+    //required the jar file in the dependencies folder + import of the library
+    Connection con;
+    PreparedStatement pst;
+    public void connect(){
+        try{
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            con = DriverManager.getConnection("jdbc:mysql://localhost/CRUDcryptodb","root","");
+            System.out.println("Successfully connected via jdbc Driver to the server    \n");
+        }
+        catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+        catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void table_load(){
+        try{
+            pst = con.prepareStatement("select * from employee");
+            ResultSet rs = pst.executeQuery();
+            table1.setModel(DbUtils.resultSetToTableModel(rs));
+            //abbiamo importato mediante dipendenza una libreria esterna rs2xml.jar
+            //che presenta il metodo DbUtils per creare e riempire dinamicamente una tabella Jtable.
+
+        }catch(SQLException e2){
+            e2.printStackTrace();
+        }
+    }
+
+    public Employee() {
+        connect();
+        table_load();
+        saveButton1.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+
+                String empname,salary,mobile;
+
+                empname = nametextfield.getText();
+                salary = salarytextfield.getText();
+                mobile = mobiletextfield.getText();
+
+                try {
+                    //TODO:control the input variable
+                    pst = con.prepareStatement("insert into employee(empname,salary,mobile) values (?,?,?)");
+                    pst.setString(1,empname);
+                    pst.setString(2,salary);
+                    pst.setString(3,mobile);
+                    pst.executeUpdate();
+                    table_load();
+                    JOptionPane.showMessageDialog(null,"Record added");
+                    nametextfield.setText(""); //risetta vuoto i text fields
+                    salarytextfield.setText("");
+                    mobiletextfield.setText("");
+                    nametextfield.requestFocus(); // fa restare il cursore in quel textfield requestFocus
+
+                } catch(SQLException e1) {
+                    e1.printStackTrace();
+                }
+
+
+            }
+        });
+        searchbutton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                try {
+                    //TODO:control the input variable
+                    pst = con.prepareStatement("select * from employee where salary <=?");
+                    pst.setString(1,searchtextfield.getText());
+                    ResultSet rs = pst.executeQuery();
+                    table1.setModel(DbUtils.resultSetToTableModel(rs));
+                    searchtextfield.setText("");
+
+                }catch (SQLException e3){
+                    e3.printStackTrace();
+                }
+
+            }
+        });
+        updatebutton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                try {
+                    //TODO:control the input variable
+                    pst = con.prepareStatement("UPDATE employee set salary = ? where id= ?");
+                    pst.setString(1,salaryupdatefield.getText());
+                    pst.setString(2,idupdatefield.getText());
+                    pst.executeUpdate();
+                    table_load();
+                    salaryupdatefield.setText("");
+                    idupdatefield.setText("");
+                    JOptionPane.showMessageDialog(null,"Update completed");
+
+                } catch(SQLException e4){
+                    e4.printStackTrace();
+                }
+
+
+            }
+        });
+    }
+
+    private void createUIComponents() {
+    }
+}
